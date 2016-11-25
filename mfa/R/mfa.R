5b835e4faaec25e0d8822233b0f4ffd9c274e078
Anna Liu <liuannaceo@gmail.com>
2016-11-24 23:45:43 -0800

    encapsulate function mfa

--- mfa/R/mfa.R
+++ mfa/R/mfa.R
@@ -1,6 +1,7 @@
 #' mfa
 #'
-#' mfa
+#' Multiple Factor Analysis
+#' 
 #' @include classes.R
 #' @param data data set (matrix or data frame).
 #' @param sets list of vectors indicating the sets of variables (i.e. the blocks).
@@ -21,50 +22,15 @@
 #' @importFrom methods new
 #' @export
 
+# main function
 mfa = function(data, sets, ncomps = NULL, center = TRUE, scale = TRUE){
   # main function
   data1 = preprocess(data,center,scale)
-  tables = split_tables(data1,sets)
-  a = alpha_weight(tables)
-  A = weight_matrix(tables, a)
-  M = diag(rep(1/nrow(data1), nrow(data1))) #diagonal mass matrix
-  W = as.matrix(data1)
-  W2 = sqrt(M)%*%W%*%sqrt(A)
-  GSVD = svd(W2)
-  M2 = diag(1/sqrt(diag(M)))
-  A2 = diag(1/sqrt(diag(A)))
-  P = M2%*%GSVD$u
-  Q = A2%*%GSVD$v
-  d = GSVD$d
-  eigenvalues = d
-  cfs = P%*%diag(d)
-  pfs = partial_factor_score(tables, sets, a, Q, ncomps)
-  if (is.null(ncomps)){
-    new(Class = "mfa",
-        ncomps = length(d),
-        sets = sets,
-        center = center,
-        scale = scale,
-        eigenvalues = eigenvalues,
-        cfs = cfs,
-        pfs = pfs,
-        loadings = Q,
-        a_weights = diag(A))
-  }else{
-    new(Class = "mfa",
-        ncomps = ncomps,
-        sets = sets,
-        center = center,
-        scale = scale,
-        eigenvalues = eigenvalues[1:ncomps],
-        cfs = cfs[,1:ncomps],
-        pfs = pfs,
-        loadings = Q[,1:ncomps],
-        a_weights = diag(A))
-  }
-  
+  res = gsvd_mfa(data1,sets,ncomps)
+  create_mfa(res, sets, center, scale, ncomps)
 }
 
+# helper functions for mfa
 preprocess = function(data, center=TRUE, scale=TRUE){
   # preprocessing: center + normalize
   d = scale(as.matrix(data), center = center, scale = scale)
@@ -105,3 +71,51 @@ partial_factor_score = function(tables, set, a, Q, ncomps=NULL){
   return(pfs)
 }
 
+gsvd_mfa = function(data, sets, ncomps){
+  # GSVD for mfa
+  tables = split_tables(data,sets)
+  a = alpha_weight(tables)
+  A = weight_matrix(tables, a)
+  M = diag(rep(1/nrow(data), nrow(data))) #diagonal mass matrix
+  W = as.matrix(data)
+  W2 = sqrt(M)%*%W%*%sqrt(A)
+  GSVD = svd(W2)
+  M2 = diag(1/sqrt(diag(M)))
+  A2 = diag(1/sqrt(diag(A)))
+  P = M2%*%GSVD$u
+  Q = A2%*%GSVD$v
+  d = GSVD$d
+  eigenvalues = d^2
+  cfs = P%*%diag(d)
+  pfs = partial_factor_score(tables, sets, a, Q, ncomps)
+  return(list(d=d,eigenvalues=eigenvalues,cfs=cfs,pfs=pfs,Q=Q,A=A))
+}
+
+create_mfa = function(res, sets, center, scale, ncomps){
+  # create mfa class object
+  if (is.null(ncomps)){
+    new(Class = "mfa",
+        ncomps = length(res$eigenvalues),
+        sets = sets,
+        center = center,
+        scale = scale,
+        singularValue = res$d,
+        eigenvalues = res$eigenvalues,
+        cfs = res$cfs,
+        pfs = res$pfs,
+        loadings = res$Q,
+        a_weights = diag(res$A))
+  }else{
+    new(Class = "mfa",
+        ncomps = ncomps,
+        sets = sets,
+        center = center,
+        scale = scale,
+        singularValue = res$d,
+        eigenvalues = res$eigenvalues[1:ncomps],
+        cfs = res$cfs[,1:ncomps],
+        pfs = res$pfs,
+        loadings = res$Q[,1:ncomps],
+        a_weights = diag(res$A))
+  }
+}
