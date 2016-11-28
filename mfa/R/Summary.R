7e495d4b24203370b7d9d6e1a7f5a2dba76932fe
Ray1521 <442060536@qq.com>
2016-11-28 12:00:51 -0800

    Update and rename summaries of eigenvalues.R to Summary.R

--- mfa/R/Summary.R
+++ mfa/R/Summary.R
@@ -2,8 +2,14 @@
 #'
 #' returns a table with the singular values, the eigenvalues, cumulative, percentage of intertia, cumulative percentage of inertia, for all the extracted components.
 #'
+#' s: the vector of singular values
+#' e: the vector of eigenvalues
+#' n: the length of s
+#' i: the percentage of inertia
+#' c1: cumulative sums of eigenvalues
+#' C2: cumulative sums of the percentage of inertia
+#' sum: matrix of Eigenvalues and Percentage of Explained Inertia of the MFA
 #' @return a table
-#'
 #'
 sumeigen <- function(Mfa){
   s = Mfa@singularValue
@@ -12,7 +18,7 @@ sumeigen <- function(Mfa){
   c1 = cumsum(e)
   i = round(e/c1[n]*100)
   c2 = cumsum(i)
-  sum <- rbind( s, e, c1, i, c2)
+  sum <- rbind(s, e, c1, i, c2)
   colnames(sum) <- 1:n
   rownames(sum) <- c("Singular value(δ)", "Eigenvalue(λ = δ^2)", "Cumulative", "%Inertia(τ)", "Cumulative")
   sum = as.table(sum)
